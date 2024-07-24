b3b3b52f979bfeb0fa90f73fee6782176c506591
Havpek <151518455+Havpek@users.noreply.github.com>
2024-07-24 22:23:55 +0600

    fix

--- test_API.py
+++ test_API.py
@@ -3,44 +3,38 @@ import json
 from constance import *
 import pytest
 
-
-
-path_1 = "/contenthub/ajaxgetcontenthubdata?hubtype=freetoplay"
-path_2 = "/freelicense/addfreelicense/674005"
-path_3 = "/IPlayerService/GetOwnedGames/v0001/?"
-
- 
-    
+   
 def test_get_info():
-        responce = requests.get(Base_URL + "/dynamicstore/userdata/?id=1714906334&cc=RU&v=1&")
+        responce = requests.get(Base_URL + path_info)
         assert responce.json
         assert responce.status_code == 200
     
 def test_free_games():
         free_games = {len: id}
-        responce = requests.get(Base_URL + path_1, params=free_games)
+        responce = requests.get(Base_URL + path_free, params=free_games)
         assert responce.json()
         assert responce.status_code == 200
     
 def test_add_email():
         responce = requests.post(
-            Base_URL + "/join/ajaxverifyemail", payload_add_mail)
+            Base_URL + path_mail, payload_add_mail)
         assert responce.json()
         assert responce.status_code == 200
     
 def test_add_game():
         responce = requests.post(
-            Base_URL + path_2, "ajax=true&session\r\nid=5f08c79b7aac7d651c2aa4c2\r\n")
+            Base_URL + path_add)
         assert responce.status_code == 200
            
 def test_get_list():
     responce = requests.get(
-        Special_URL + "/ISteamApps/GetAppList/v2/")
+        Special_URL + path_list)
     assert responce.json()
+    assert responce.status_code == 200
     
 def test_get_having_list():
     
     responce = requests.get(
-           Special_URL + path_3, "key=04AB93DF9A5678857B0742A66A0C7BDA&steamid=76561198070806378")
+           Special_URL + path_having)
     assert responce.json()
     assert responce.status_code == 200
