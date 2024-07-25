c703992f94358ae7de99b75c0adb406c0ba866f6
Havpek <151518455+Havpek@users.noreply.github.com>
2024-07-25 13:37:49 +0700

    add test

--- test/MainPage.py
+++ test/MainPage.py
@@ -25,3 +25,30 @@ class MainPage:
             EC.presence_of_element_located((By.CSS_SELECTOR, 'span.pulldown.global_action_link.persona_name_text_content'))
         )
         return self.browser.find_element(By.CSS_SELECTOR, 'span.pulldown.global_action_link.persona_name_text_content').text
+    
+    def add_to_cart(self):
+        WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div.search_results_filtered_warning'))
+            )
+        self.browser.find_element(By.CSS_SELECTOR, "a[data-gpnav='item']").click()
+        self.browser.find_element(By.CSS_SELECTOR, 'a.btn_green_steamui.btn_medium').click()
+        WebDriverWait(self.browser, 10).until(
+            EC.element_to_be_clickable((
+                By.CSS_SELECTOR, 'button.DialogButton._DialogLayout.Primary.Focusable')))
+        self.browser.find_element(
+            By.CSS_SELECTOR, 'button.DialogButton._DialogLayout.Primary.Focusable').click()
+        return self.browser.find_element(By.CSS_SELECTOR, "div.pVXX8Pzc4JbT40TP4RwRG").text
+    
+    def dell_from_cart(self):
+        WebDriverWait(self.browser, 10).until(
+            EC.element_to_be_clickable((
+                By.CSS_SELECTOR, 'div._33j4SwfO2YH9eI6qV9BKaL.Panel.Focusable')))
+        self.browser.find_element(
+            By.CSS_SELECTOR, 'div._33j4SwfO2YH9eI6qV9BKaL.Panel.Focusable').click()
+
+        WebDriverWait(self.browser, 10).until(
+            EC.text_to_be_present_in_element((
+                By.CSS_SELECTOR, 'div._17GFdSD2pc0BquZk5cejg8.Panel.Focusable > div:nth-child(1)'),
+                  "Ваша корзина пуста."))
+        return self.browser.find_element(
+            By.CSS_SELECTOR, 'div._17GFdSD2pc0BquZk5cejg8.Panel.Focusable > div:nth-child(1)').text
--- test/test_UI.py
+++ test/test_UI.py
@@ -1,7 +1,4 @@
 from MainPage import MainPage
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from data import *
 
 def test_assertion(chrome_browser):
@@ -9,3 +6,24 @@ def test_assertion(chrome_browser):
     main_page.filling_in_the_fields('testingfechures', '553085530855308g')
     user = main_page.click_submit_button()
     assert user == 'test250995'
+
+def test_add_game_to_cart(chrome_browser):
+    main_page = MainPage(chrome_browser)
+    main_page.filling_in_the_fields('testingfechures', '553085530855308g')
+    user = main_page.click_submit_button()
+    f_game = 'rust'
+    main_page.find_game(f_game)
+    game = main_page.add_to_cart()
+    # очистка тестового пространства - удаление игры
+    main_page.dell_from_cart()
+    assert game.lower() == f_game.lower()
+
+def test_del_game_from_cart(chrome_browser):
+    main_page = MainPage(chrome_browser)
+    main_page.filling_in_the_fields('testingfechures', '553085530855308g')
+    user = main_page.click_submit_button()
+    f_game = "No Man's Sky"
+    main_page.find_game(f_game)
+    main_page.add_to_cart()
+    empty_cart = main_page.dell_from_cart()
+    assert empty_cart == 'Ваша корзина пуста.'
